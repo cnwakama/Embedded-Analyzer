fafc28245ad11be8cbf3d8036ecbb7fa8315565e
Chibuzo Nwakama <chibuzo-nwakama@uiowa.edu>
2018-04-30 13:55:02 -0500

    removed Folder- Embedded Analyzer

--- Embedded Analyzer/Final Project/Embedproj/Embedproj/Embedproj/Debug/Analysis Tools/pair.d
+++
@@ -1,4 +0,0 @@
-Analysis Tools/pair.d Analysis Tools/pair.o: ../Analysis\ Tools/pair.c \
- ../Analysis\ Tools/pair.h
-
-../Analysis\ Tools/pair.h:
--- Embedded Analyzer/Final Project/Embedproj/Embedproj/Embedproj/Debug/fftavr/ffft.d
+++
@@ -1,3 +0,0 @@
-fftavr/ffft.d fftavr/ffft.o: ../fftavr/ffft.S ../fftavr/ffft.h
-
-../fftavr/ffft.h:
